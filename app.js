20adb2ae2c4d9779998d98b4ca3cf31c4f0b7a29
AnnQK <annhuddy13@yandex.ru>
2022-02-08 18:10:02 +0300

    feat: implement variables to js

---
+++ app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const btnCalc = document.querySelector('.section__form__box__btn');
+const inputRadio = document.querySelectorAll('input[name="coffee"]');
+const numberOfCups = document.querySelector('.input-number');
+const amountOfCoffee = document.querySelector('.section__output__text');
+const messageToUser = document.querySelector('.section__output__result');
