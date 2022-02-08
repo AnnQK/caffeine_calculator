d500c6920e06dcb4e90ecefe035dbc8bfa44a32f
AnnQK <annhuddy13@yandex.ru>
2022-02-08 18:12:53 +0300

    feat: implement btn event

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const numberOfCups = document.querySelector('.input-number');
 const amountOfCoffee = document.querySelector('.section__output__text');
 const messageToUser = document.querySelector('.section__output__result');
 
+const cafDoses = {
+  espresso: 80,
+  alt: 150,
+  instant: 250,
+};
+
 function checkRadioBtn() {
   let selectedType;
   for (let radioBtn of inputRadio) {
@@ -22,3 +28,19 @@ function calcCaffeine() {
   const totalCaf = dose * numberOfCups.value;
   return totalCaf;
 }
+
+function checkCafNorm(caffeine) {
+  return caffeine < 200
+    ? 'You can drink enough two cups of espresso or one alternative.'
+    : caffeine < 400
+    ? 'Slow down! Drink more water.'
+    : 'Whoa! You have drank too much coffee today!';
+}
+
+function textFormat() {
+  const dose = calcCaffeine();
+  amountOfCoffee.textContent = `Total amount of caffeine: ${dose} mg.`;
+  messageToUser.textContent = checkCafNorm(dose);
+}
+
+btnCalc.addEventListener('click', textFormat);
