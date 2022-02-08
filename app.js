fbea320ecb12311ed830e6b3050f21676430a9a6
AnnQK <annhuddy13@yandex.ru>
2022-02-08 18:11:33 +0300

    feat: implement functionality for inputs

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ const inputRadio = document.querySelectorAll('input[name="coffee"]');
 const numberOfCups = document.querySelector('.input-number');
 const amountOfCoffee = document.querySelector('.section__output__text');
 const messageToUser = document.querySelector('.section__output__result');
+
+function checkRadioBtn() {
+  let selectedType;
+  for (let radioBtn of inputRadio) {
+    if (radioBtn.checked) {
+      selectedType = radioBtn.id;
+    }
+  }
+  return selectedType;
+}
+
+function calcCaffeine() {
+  const result = checkRadioBtn();
+  const dose = cafDoses[result];
+  const totalCaf = dose * numberOfCups.value;
+  return totalCaf;
+}
